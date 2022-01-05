31a6d7819598b85610d32f1f5f1dee0e89dbe464
Marcus Hüsemann <mchusemann@gmail.com>
2022-01-05 13:13:25 -0300

    fix(conta.py): muda nomes dos metodos para ingles

--- account.py
+++ account.py
@@ -9,23 +9,23 @@ class Account:
         self.__balance = balance
         self.__limit = limit
 
-    def extrato(self):
+    def extract(self):
         print(f"Saldo {self.__balance} do titular {self.__owner}")
 
-    def deposita(self, value):
+    def deposit(self, value):
         self.__balance += value
 
-    def saca(self, value):
-        balance_antigo = self.__balance
+    def withdraw(self, value):
+        old_balance = self.__balance
         if(self.__balance >= value):
             self.__balance -= value
             print(f"Valor sacado: {value}")
-            print(f"Saldo antigo: {balance_antigo}")
+            print(f"Saldo antigo: {old_balance}")
             print(f"Saldo atual: {self.__balance}")
 
-    def transfere(self, value, destination):
-        self.saca(value)
-        destination.deposita(value)
+    def transfer(self, value, destination):
+        self.withdraw(value)
+        destination.deposit(value)
 
     @property
     def balance(self):
@@ -47,9 +47,9 @@ class Account:
 if(__name__ == '__main__'):
     conta = Account(1, 'Marcus', 10)
     conta2 = Account(2, 'Mayra', 10)
-    conta.transfere(10, conta2)
-    print(conta.extrato())
-    print(conta2.extrato())
+    conta.transfer(10, conta2)
+    print(conta.extract())
+    print(conta2.extract())
     new_date = Date(12, 2, 1992)
     new_date.format_date()
     client = Client('Marcus')
