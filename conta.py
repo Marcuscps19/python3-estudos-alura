6dd00d6757e6cc920899b6c6b8e46475744264d9
Marcus Hüsemann <mchusemann@gmail.com>
2022-01-05 12:07:57 -0300

    fix(conta.py): muda nomes das variaveis para ingles

--- conta.py
+++ conta.py
@@ -2,24 +2,24 @@ from date import Date
 
 
 class Conta:
-    def __init__(self, numero, titular, saldo, limite=1000.0):
-        self.__numero = numero
-        self.__titular = titular
-        self.__saldo = saldo
-        self.__limite = limite
+    def __init__(self, number, owner, balance, limit=1000.0):
+        self.__number = number
+        self.__owner = owner
+        self.__balance = balance
+        self.__limit = limit
 
     def extrato(self):
-        print(f"Saldo {self.__saldo} do titular {self.__titular}")
+        print(f"Saldo {self.__balance} do titular {self.__owner}")
 
     def deposita(self, value):
-        self.__saldo += value
+        self.__balance += value
 
     def saca(self, value):
-        saldo_antigo = self.__saldo
-        if(self.__saldo >= value):
-            self.__saldo -= value
+        balance_antigo = self.__balance
+        if(self.__balance >= value):
+            self.__balance -= value
             print(f"Valor sacado: {value}")
-            print(f"Saldo antigo: {saldo_antigo}\nSaldo atual: {self.__saldo}")
+            print(f"Saldo antigo: {balance_antigo}\nSaldo atual: {self.__balance}")
 
     # def transfere(self, value, )
 
