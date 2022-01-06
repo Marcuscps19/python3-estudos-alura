8e197f0d1d7621ee188b02b2ff92d749d01bcf40
Marcus Hüsemann <mchusemann@gmail.com>
2022-01-06 16:20:32 -0300

    feat(employees): adiciona classes para entendimento de herança multipla

---
+++ py3-avancando-na-linguagem/employees/alura.py
@@ -0,0 +1,7 @@
+from employee import Employee
+
+
+class Alura(Employee):
+
+    def search_unanswered_questions(self):
+        print('Mostrando perguntas não respondidas do fórum')
---
+++ py3-avancando-na-linguagem/employees/employee.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self, name):
+        self.name = name
+
+    def log_hours(self, hours):
+        print('Horas registradas.')
+
+    def show_tasks(self):
+        print('Fez muita coisa...')
